7c65dd418cfdff4d12990c890850ae3da44e4b8f
MichalOndrejka <michalkoo700@gmail.com>
2022-04-01 19:46:52 +0200

    Added float test and fixed one nthroor test

--- math_functions_tests.py
+++ math_functions_tests.py
@@ -7,18 +7,21 @@ class test_calc(unittest.TestCase):
         self.assertEqual(calculator.addition(0,0),0)
         self.assertEqual(calculator.addition(-1,2),1)
         self.assertEqual(calculator.addition(-1,-1),-2)
+        self.assertEqual(calculator.addition(1.5,0.25),1.75)
 
-    def test_substract(self):
+    def test_subtract(self):
         self.assertEqual(calculator.subtract(20,15),5)
         self.assertEqual(calculator.subtract(0,0),0)
         self.assertEqual(calculator.subtract(-5,8),-13)
         self.assertEqual(calculator.subtract(-1,-1),0)
+        self.assertEqual(calculator.addition(1.5,0.25),1.25)
     
     def test_multiply(self):
         self.assertEqual(calculator.multiply(2,15),30)
         self.assertEqual(calculator.multiply(0,8),0)
         self.assertEqual(calculator.multiply(-1,2),-2)
         self.assertEqual(calculator.multiply(-10,-10),100)
+        self.assertEqual(calculator.addition(1.5,0.25),0.3125)
     
     def test_divide(self):
         self.assertEqual(calculator.divide(100,20),5)
@@ -26,6 +29,7 @@ class test_calc(unittest.TestCase):
         self.assertEqual(calculator.divide(-10,-2),5)
         self.assertEqual(calculator.divide(9,-3),-3)
         self.assertRaises(ValueError,calculator.divide, 5,0)
+        self.assertEqual(calculator.addition(1.5,0.25),6)
 
     def test_exponentiate(self):
         self.assertEqual(calculator.exponentiate(10,2),100)
@@ -33,15 +37,17 @@ class test_calc(unittest.TestCase):
         self.assertEqual(calculator.exponentiate(-4,2),16)
         self.assertEqual(calculator.exponentiate(-4,3),-64)
         self.assertEqual(calculator.exponentiate(100,0),1)
-        self.assertRaises(ValueError,calculator.exponentiate, 2,-3)
+        self.assertRaises(calculator.exponentiate(2,-3),1/(2**3))
+        self.assertEqual(calculator.exponentiate(0.25,1.5),0.25**1.5)
 
     def test_nthroot(self):
         self.assertEqual(calculator.nthroot(100,2),10)
-        self.assertEqual(calculator.nthroot(64,3),2)
+        self.assertEqual(calculator.nthroot(64,3),4)
         self.assertRaises(ValueError,calculator.nthroot, -4,2)
         self.assertEqual(calculator.nthroot(-8,3),-2)
         self.assertEqual(calculator.nthroot(0,2),0)
-        self.assertEqual(calculator.nthroot(70.5,2),8.396427811873332)
+        self.assertEqual(calculator.nthroot(70.5,2),70.5**(1/2))
+        self.assertEqual(calculator.exponentiate(1.75,1.5),1.75**1.5)
 
     def test_factorial(self):
         self.assertEqual(calculator.factorial(7),5040)
